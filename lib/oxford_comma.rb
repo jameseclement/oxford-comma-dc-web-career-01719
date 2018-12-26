a12e686c3542a5c9e64cf214f79620a9915bed22
jameseclement <jameseclement@gmail.com>
2018-12-26 20:55:12 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -2,10 +2,11 @@ def oxford_comma(array)
 length = array.length
 
 if length = 1
+  array.join(", ")
   
   if length = 2
     
-    if lenght > 2
+    if length > 2
       
 without comma = array.join(", " )
 
